e1b5a3dd734d537c853554320331cbeaa459b787
nworb95 <nworb95@gmail.com>
2021-03-09 22:12:54 -0500

    Added query sql function for brevity

--- src/helper.py
+++ src/helper.py
@@ -1,4 +1,5 @@
 import sqlite3
+from contextlib import closing
 
 
 DB_PATH = 'db/todo.db'
@@ -8,25 +9,29 @@ COMPLETED = 'Completed'
 
 
 def init_table():
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    query = open('db/sql/create_items.sql', 'r').read()
-    print(query)
-    c.execute(query)
-    conn.commit()
-    c.close()
-    conn.close()
+    query_sqlite(open('db/sql/create_items.sql', 'r').read())
     print("Table initialized!")
+
+
+def query_sqlite(query, query_type='execute', **kwargs):
+    conn = sqlite3.connect(DB_PATH)
+    result = None
+    with closing(conn.cursor()) as c:
+        if query_type == 'execute':
+            if kwargs.get('item'):
+                c.execute(query, kwargs.get('item'))
+            else:
+                c.execute(query)
+        if query_type == 'fetchall':
+            c.execute(query)
+            result = c.fetchall()
+    conn.commit()
+    return result
 
 
 def add_to_list(item):
     try:
-        conn = sqlite3.connect(DB_PATH)
-        c = conn.cursor()
-        c.execute('insert into items(item, status) values(?,?)', (item, NOT_STARTED))
-        conn.commit()
-        c.close()
-        conn.close()
+        query_sqlite('insert into items(item, status) values(?,?)', item=(item, NOT_STARTED))
         return {"item": item, "status": NOT_STARTED}
     except Exception as e:
         print('Error :: ', e)
@@ -35,10 +40,7 @@ def add_to_list(item):
 
 def get_all_items():
     try:
-        conn = sqlite3.connect(DB_PATH)
-        c = conn.cursor()
-        c.execute('select * from items')
-        rows = c.fetchall()
+        rows = query_sqlite('select * from items', query_type='fetchall')
         return {"count": len(rows), "items": rows }
     except Exception as e:
         print('Error :: ', e)
@@ -68,10 +70,7 @@ def update_status(item, status):
         print("Invalid Status: " + status)
         return None
     try:
-        conn = sqlite3.connect(DB_PATH)
-        c = conn.cursor()
-        c.execute('update items set status=? where item=?', (status, item))
-        conn.commit()
+        query_sqlite('update items set status=? where item=?', (status, item))
         return {item: status}
     except Exception as e:
         print('Error :: ', e)
